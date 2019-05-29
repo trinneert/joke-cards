82fece09eca042a809a9345e1a75300ded828d34
tom t <tom@trinneer.com>
2019-05-29 10:16:25 -0700

    fixed props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
+import Joke from './Joke';
+import {
+  Jumbotron,
+  Container,
+  Row,
+  Col
+} from 'reactstrap';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      jokes: [
+        {
+          setup: "What time did the man go to the dentist?",
+          punchline: "Tooth hurt-y"
+        },
+        {
+          setup: "Did you hear about the guy who invented Lifesavers?",
+          punchline: "They say he made a mint"
+        },
+        {
+          setup: "Why do chicken coops only have two doors?",
+          punchline: "Because if they had four, they would be chicken sedans!"
+        },
+        {
+          setup: "Why did the Clydesdale give the pony a glass of water?",
+          punchline: "Because he was a little horse!"
+        }
+      ]
+    }
+  }
 
-export default App;
+  render() {
+    let jokeCards = this.state.jokes.map(humor => {
+      return (
+        <Col className="p-3" sm="4">
+          <Joke humor={humor} />
+        </Col>
+      )
+    })
+    return (
+      <div>
+        <Jumbotron>
+          <h1 className="display-3">Joke Cards</h1>
+          <p className="lead">Welcome to the next billion dollar app taking the world by storm</p>
+          <hr className="my-2" />
+          <p>Prepare to be amused!</p>
+          <p className="lead">
+          </p>
+        </Jumbotron>
+        <Container fluid>
+          <Row>
+            {jokeCards}
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+};
+
---
+++ src/Joke.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {
+    Card,
+    CardBody,
+    CardTitle,
+    CardText,
+    Button,
+    Fade
+} from 'reactstrap';
+import './App.css';
+
+export default class Joke extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { fadeIn: false };
+        this.toggle = this.toggle.bind(this);
+      }
+
+    render() {
+        let { setup, punchline } = this.props.humor;
+        return (
+            <div>
+                <Card>
+                    <CardBody>
+                        <CardTitle>{setup}</CardTitle>
+                        <Button color="primary" onClick={this.toggle}>Laugh</Button>
+                        <Fade in={this.state.fadeIn} className='my-2'>
+                            <CardText>{punchline}</CardText>
+                        </Fade>
+                    </CardBody>
+                </Card>
+            </div>
+        );
+    }
+
+    toggle() {
+        this.setState({
+          fadeIn: !this.state.fadeIn
+        });
+    }
+};
